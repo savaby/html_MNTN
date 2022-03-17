8c6ffd8779bee6865047ff34859415ebfa6e017f
sava <sava666@mail.ru>
2022-03-17 09:44:21 +0300

    hero css & parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const openMenuIcon = document.getElementById("menu-toggle-open")
 const closeMenuIcon = document.getElementById("menu-toggle-close")
 const mobileMenu = document.getElementById("mobile-menu")
 const headerOverlay = document.getElementById("header")
-const grassImage = document.getElementById("grassland")
+const grassImage = document.getElementById("grass")
 const skyImage = document.getElementById("sky")
 const mountainsImage = document.getElementById("mountains")
 const heroData = document.getElementById("hero-data")
@@ -19,3 +19,12 @@ closeMenuIcon.addEventListener("click", () => {
     headerOverlay.classList.remove("menu-opened")
 })
 // Add parallax effect
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY
+    grassImage.style.transform = "translateY(" + -scrollPosition * 0.02 + "%)"
+    mountainsImage.style.transform =
+        "translateY(" + -scrollPosition * 0.04 + "%)"
+    skyImage.style.transform = "translateY(" + -scrollPosition * 0.01 + "%)"
+    heroData.style.transform = "translateY(" + scrollPosition * 0.03 + "%)"
+    heroData.style.opacity = 1 - scrollPosition * 0.002
+})
