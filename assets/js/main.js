ba36710db3f2ed706d4c43245f1c3817fcccb742
sava <sava666@mail.ru>
2022-03-17 08:59:57 +0300

    header css & js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 // Variable declarations
-const openMenuIcon = document.getElementById('menu-toggle-open');
-const closeMenuIcon = document.getElementById('menu-toggle-close');
-const mobileMenu = document.getElementById('mobile-menu');
-const headerOverlay = document.getElementById('header');
-const grassImage = document.getElementById('grassland');
-const skyImage = document.getElementById('sky');
-const mountainsImage = document.getElementById('mountains');
-const heroData = document.getElementById('hero-data');
+const openMenuIcon = document.getElementById("menu-toggle-open")
+const closeMenuIcon = document.getElementById("menu-toggle-close")
+const mobileMenu = document.getElementById("mobile-menu")
+const headerOverlay = document.getElementById("header")
+const grassImage = document.getElementById("grassland")
+const skyImage = document.getElementById("sky")
+const mountainsImage = document.getElementById("mountains")
+const heroData = document.getElementById("hero-data")
 
 // Open menu & add overlay
-
+openMenuIcon.addEventListener("click", () => {
+    mobileMenu.classList.add("menu-opened")
+    headerOverlay.classList.add("menu-opened")
+})
 // Close menu & remove overlay
-
+closeMenuIcon.addEventListener("click", () => {
+    mobileMenu.classList.remove("menu-opened")
+    headerOverlay.classList.remove("menu-opened")
+})
 // Add parallax effect
